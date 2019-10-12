740193789ffd8c4608e8f6ee3df25463c4a910c1
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2019-10-11 21:45:03 -0400

    MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
+import MySquad from './Components/Team';
+import Form from './Components/Form';
 import './App.css';
 
 function App() {
+  const [squadMembers, setSquadMembers] = useState([{
+    id: 1,
+    name: "Courtney Jackson",
+    role: "Web Developer",
+    email: "courtney.jackson08234@gmail.com"
+  }])
+
+  const addMemberFn = squadMember =>{
+    const newSquadMember={
+      id: Date.now(),
+      name: squadMember.name,
+      role: squadMember.role,
+      email: squadMember.email
+    }
+    setSquadMembers([...squadMembers, newSquadMember]);
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Add a New Squad Member</h1>
+        <Form addMemberFn={addMemberFn}/>
+        <MySquad squadMembers={squadMembers}/>
       </header>
     </div>
   );
